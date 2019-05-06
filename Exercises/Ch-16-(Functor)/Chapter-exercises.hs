2adfeb685b8b4900fce08dfd579eef3f702665e7
Onur Rıdvanoğlu <onurridvanoglu@Onurs-MacBook-Pro.local>
2019-05-06 15:11:43 +0300

    Added Rearrange exercises

--- Exercises/Ch-16-(Functor)/Chapter-exercises.hs
+++ Exercises/Ch-16-(Functor)/Chapter-exercises.hs
@@ -18,3 +18,22 @@ instance Functor BoolAndMaybeSomethingElse where
     fmap f (Truish a) = Truish (f a)
     fmap _ Falsish = Falsish
 
+-- Rearrange the arguments
+-- 1
+data Sum a b = First a | Second b
+instance Functor (Sum e) where 
+    fmap _ (First a) = First a
+    fmap f (Second b) = Second (f b)
+
+-- 2
+data Company a b c = DeepBlue a c | Something b
+instance Functor (Company e e') where 
+    fmap _ (Something b) = Something b
+    fmap f (DeepBlue a c) = DeepBlue a (f c)
+
+-- 3
+data More a b = L a b a | R b a b deriving (Eq, Show)
+instance Functor (More a) where 
+    fmap f (L a b a') = L (f a) b (f a') 
+    fmap f (R b a b') = R b (f a) b'
+
