9b4a32041d5904cea3866a0e26d4b8208315d411
Onur Rıdvanoğlu <onurridvanoglu@Onurs-MacBook-Pro.local>
2019-05-06 18:08:26 +0300

    fix typo,

--- Exercises/Ch-16-(Functor)/intermission1/app/Main.hs
+++ Exercises/Ch-16-(Functor)/intermission1/app/Main.hs
@@ -12,19 +12,23 @@ instance Arbitrary a => Arbitrary (Identity a) where
         a <- arbitrary
         return $ Identity a
 
-functorIdentity :: (Functor f, Eq (f a)) => (a -> b) -> (b -> c) -> f a -> Bool
+functorIdentity :: (Functor f, Eq (f a)) =>  f a -> Bool
 functorIdentity f = fmap id f == f
 
 functorCompose :: (Functor f, Eq (f c)) => (a -> b) -> (b -> c) -> f a -> Bool
-functorCompose f g x = (fmap g (fmap f x)) == (fmap (f . g) x)
+functorCompose f g x = (fmap g (fmap f x)) == (fmap (g . f) x)
+
+functorCompose' :: (Functor f, Eq (f c)) => Fun a b -> Fun b c -> f a -> Bool
+functorCompose' (Fun _ f) (Fun _ g) x = (fmap g (fmap f x)) == (fmap (g . f) x)
 
 type IdentityStr = Identity String 
-type IdCompose = IdentityStr -> IdentityStr -> IdentityStr -> Bool
+type FunStr = Fun String String
+type IdCompose = FunStr -> FunStr -> IdentityStr -> Bool
 
 main :: IO ()
 main = do
-    quickCheck (functorIdentity :: Identity a -> Bool)
-    quickCheck (functorCompose :: IdCompose)
+    quickCheck (functorIdentity :: Identity String -> Bool)
+    quickCheck (functorCompose' :: IdCompose)
 
 
 
