2cb0797b4ca9f6a18dc03c9f92c285686466712a
Onur Rıdvanoğlu <onurridvanoglu@Onurs-MacBook-Pro.local>
2019-05-06 16:15:56 +0300

    complete Functor instances (except 11)

--- Exercises/Ch-16-(Functor)/Chapter-exercises.hs
+++ Exercises/Ch-16-(Functor)/Chapter-exercises.hs
@@ -45,4 +45,47 @@ instance Functor (Quant a) where
     fmap f (Desk a) = Desk a
     fmap f (Bloor b) = Bloor (f b)
 
+-- 2
+data K a b = K a deriving (Eq, Show)
+instance Functor (K a) where
+    fmap f (K a) = K a
 
+-- 4 
+data EvilGoateeConst a b = GoatyConst b deriving (Eq, Show)
+instance Functor (EvilGoateeConst a) where
+    fmap f (GoatyConst b) = GoatyConst (f b)
+
+-- 5
+data LiftItOut f a = LiftItOut (f a)
+instance (Functor f) => Functor (LiftItOut f) where
+    fmap f (LiftItOut a) = LiftItOut (fmap f a)
+
+-- 6
+data Parappa f g a = DaWrappa (f a) (g a)
+instance (Functor f, Functor g) => Functor (Parappa f g) where
+    fmap f (DaWrappa g a) = DaWrappa (fmap f g) (fmap f a)
+
+-- 7
+data IgnoreOne f g a b = IgnoringSomething (f a) (g b)
+instance (Functor f, Functor g) => Functor (IgnoreOne f g a) where
+    fmap f (IgnoringSomething g a) = IgnoringSomething g (fmap f a)
+
+-- 8
+data Notorious g o a t = Notorious (g o) (g a) (g t)
+instance Functor g => Functor (Notorious g a b) where
+    fmap g (Notorious o a t) = Notorious o a (fmap g t)
+    
+-- 9
+data List a = Nil | Cons a (List a) deriving (Eq, Show)
+instance Functor List where
+    fmap _ Nil = Nil
+    fmap f (Cons a l) = Cons (f a) (fmap f l)
+
+-- 10
+data GoatLord a = NoGoat | OneGoat a | MoreGoats (GoatLord a) (GoatLord a) (GoatLord a)
+instance Functor GoatLord where
+    fmap _ NoGoat = NoGoat
+    fmap f (OneGoat a) = OneGoat (f a)
+    fmap f (MoreGoats l m r) = MoreGoats (fmap f l) (fmap f m) (fmap f r)
+
+-- 11 
