713a3199cf9d3d9a84411b7e5464766701a0dc28
Onur Rıdvanoğlu <onurridvanoglu@Onurs-MacBook-Pro.local>
2019-05-08 23:27:59 +0300

    add comment

--- Exercises/Ch-17(Applicative)/Combinations.hs
+++ Exercises/Ch-17(Applicative)/Combinations.hs
@@ -1,8 +1,14 @@
 -- Combinations Exercises
-
 import Control.Applicative (liftA3)
-stops, vowels :: String 
+
+stops :: String 
+vowels :: String 
+
 stops = "pbtdkg" 
 vowels = "aeiou"
+
 combos :: [a] -> [b] -> [c] -> [(a, b, c)] 
 combos = liftA3 (,,) 
+-- What the LiftA3 is doing here is that
+-- (,,) <$> "a" <*> "b" <*> "c"
+
--- Exercises/Ch-17(Applicative)/Validation.hs
+++ Exercises/Ch-17(Applicative)/Validation.hs
@@ -1,3 +1,5 @@
+module Validation where
+
 data Validation err a = Failure err | Success a deriving (Eq, Show) 
 
 validToEither :: Validation e a -> Either e a
@@ -9,3 +11,16 @@ eitherToValid (Left err) = Failure err
 eitherToValid (Right a) = Success a
 
 
+data Errors = DividedByZero | StackOverflow | MooglesChewedWires deriving (Eq, Show)
+
+success = Success (+1) <*> Success 1
+success == Success 2 
+
+failure = Success (+1) <*> Failure [StackOverflow]
+failure == Failure [StackOverflow]
+
+failure'= Failure [StackOverflow] <*> Success (+1)
+failure' == Failure [StackOverflow] 
+
+failures = Failure [MooglesChewedWires] <*> Failure [StackOverflow]
+failures == Failure [MooglesChewedWires, StackOverflow]
