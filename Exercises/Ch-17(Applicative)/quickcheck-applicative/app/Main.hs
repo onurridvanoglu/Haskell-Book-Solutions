8686ea4c328a1351ff9626a6f29c5af0a4ae3a00
Onur Rıdvanoğlu <onurridvanoglu@Onurs-MacBook-Pro.local>
2019-05-08 22:08:23 +0300

    fix type

--- Exercises/Ch-17(Applicative)/quickcheck-applicative/app/Main.hs
+++ Exercises/Ch-17(Applicative)/quickcheck-applicative/app/Main.hs
@@ -1,20 +1,19 @@
 module BadMonoid where
 
-
-
 import Data.Monoid
 import Test.QuickCheck
 import Test.QuickCheck.Checkers 
-import Test.QuickCheck.Classes 
         
 data Bull = Fools | Twoo deriving (Eq, Show)
         
 instance Arbitrary Bull where
     arbitrary = frequency [(1, return Fools), (1, return Twoo)]
         
+instance Semigroup Bull where
+    _ <> _ = Fools
 instance Monoid Bull where
     mempty = Fools
-    mappend _ _ = Fools
+    mappend _ _ = (<>)
         
         -- EqProp is a class available in Checkers
         -- Checkers requires an instance of EqProp in order for us to use monoid
