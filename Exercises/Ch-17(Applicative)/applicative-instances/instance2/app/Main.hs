021b89cb02628b15d2e9927e60900c0150bfdca2
Onur Rıdvanoğlu <onurridvanoglu@Onurs-MacBook-Pro.local>
2019-05-09 15:41:19 +0300

    add instances

--- Exercises/Ch-17(Applicative)/applicative-instances/instance2/app/Main.hs
+++ Exercises/Ch-17(Applicative)/applicative-instances/instance2/app/Main.hs
@@ -1,6 +1,21 @@
 module Main where
 
-import Lib
+import Control.Applicative
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
 
-main :: IO ()
-main = someFunc
+data Two a b = Two a b deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
+    (Two a b) <> (Two a' b') = Two (a <> a') (b <> b')
+
+instance (Semigroup a, Semigroup b, Monoid a, Monoid b) => Monoid (Two a b) where
+    mempty = Two mempty mempty
+    mappend = (<>)
+
+instance Functor f => Functor (Two a) where
+    fmap f (Two a b) = Two a (f b)
+
+instance Applicative
+
