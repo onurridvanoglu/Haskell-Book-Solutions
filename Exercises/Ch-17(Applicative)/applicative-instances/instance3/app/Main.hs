f4a0be84d69fcc1e4f3d2223b2855343132d26ab
Onur Rıdvanoğlu <onurridvanoglu@Onurs-MacBook-Pro.local>
2019-05-10 10:18:27 +0300

    add applicative instance

--- Exercises/Ch-17(Applicative)/applicative-instances/instance3/app/Main.hs
+++ Exercises/Ch-17(Applicative)/applicative-instances/instance3/app/Main.hs
@@ -1,6 +1,27 @@
 module Main where
 
-import Lib
+import Control.Applicative
+import Test.QuickCheck
 
-main :: IO ()
-main = someFunc
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b, Semigroup c) => Semigroup (Three a b c) where
+    (Three a b c) <> (Three x y z) = Three (a <> x) (b <> y) (c <> z)
+
+instance (Semigroup a, Semigroup b, Semigroup c, Monoid a, Monoid b, Monoid c) => Monoid (Three a b c) where
+    mempty = Three mempty mempty mempty
+    mappend = (<>)
+
+instance Functor (Three a b) where
+    fmap f (Three a b c) = Three a b (f c)
+
+instance (Monoid a, Monoid b) => Applicative (Three a b) where
+    (Three x y f) <*> (Three a b c) = Three (x <> a) (y <> b) (f c) 
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        c <- arbitrary
+        return $ Three a b c
+
