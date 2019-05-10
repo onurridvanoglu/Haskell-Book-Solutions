2d4f7f767b876c8dd9125b1c16ee07b939eab4fe
Onur Rıdvanoğlu <onurridvanoglu@Onurs-MacBook-Pro.local>
2019-05-10 17:40:43 +0300

    complete maybe monad

--- Exercises/Ch-18-(Monad)/monadmaybe.hs
+++ Exercises/Ch-18-(Monad)/monadmaybe.hs
@@ -49,3 +49,31 @@ mkSphericalCow'' name' age' weight' =
                 noNegative weight' >>=
                 \ weighty ->
                 weightCheck (Cow nammy agey weighty)
+
+f :: Maybe Integer
+f = Just 1
+
+g :: Maybe String
+g = Just "1"
+
+h :: Maybe Integer
+h = Just 10191
+
+zed :: a -> b -> c -> (a, b, c)
+zed = (,,)
+
+doSomething = do
+    a <- f
+    b <- g
+    c <- h
+    return (zed a b c)
+
+zed' :: Monad m => a -> b -> c -> m (a, b, c)
+zed' a b c =  return (a, b, c)
+
+doSomething' = do
+    a <- f
+    b <- g
+    c <- h
+    zed' a b c
+
