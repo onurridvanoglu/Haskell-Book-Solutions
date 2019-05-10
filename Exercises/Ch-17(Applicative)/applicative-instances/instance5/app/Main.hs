ca73598d126d738c9078bc2dc7cb04ce547d7c40
Onur Rıdvanoğlu <onurridvanoglu@Onurs-MacBook-Pro.local>
2019-05-10 11:53:26 +0300

    add applicative instance

--- Exercises/Ch-17(Applicative)/applicative-instances/instance5/app/Main.hs
+++ Exercises/Ch-17(Applicative)/applicative-instances/instance5/app/Main.hs
@@ -1,6 +1,31 @@
 module Main where
 
-import Lib
+import Control.Applicative
+import Test.QuickCheck
 
-main :: IO ()
-main = someFunc
+data Four a b c d = Four a b c d deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b, Semigroup c, Semigroup d) => Semigroup (Four a b c d) where
+    (Four a b c d) <> (Four a' b' c' d') = Four (a <> a') (b <> b') (c <> c') (d <> d^)
+
+instance (Semigroup a, Semigroup b, Semigroup c, Semigroup d, Monoid a, Monoid b, Monoid c, Monoid d) => Monoid (Four a b c d) where
+    mempty = Four mempty mempty mempty mempty 
+    mappend = (<>)
+
+instance Functor (Four a b c) where
+    fmap f (Four a b c d) = Four a b c (f d)
+
+instance (Monoid a, Monoid b, Monoid c) => Applicative (Four a b c) where
+    pure a = Four mempty mempty mempty a
+    (Four a b c f) <*> (Four a' b' c' d) = Four (a <> a') (b <> b') (c <> c') (f d)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) => Arbitrary (Four a b c d) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary 
+        c <- arbitrary
+        d <- arbitrary
+        return $ Four a b c d
+
+
+    
