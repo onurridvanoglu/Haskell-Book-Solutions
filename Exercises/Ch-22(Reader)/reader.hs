8b16aad2a345b02a2f7486457524569a322ae9a7
Onur Rıdvanoğlu <onurridvanoglu@Onurs-MacBook-Pro.local>
2019-05-22 22:14:10 +0300

    complete short exercise

--- Exercises/Ch-22(Reader)/reader.hs
+++ Exercises/Ch-22(Reader)/reader.hs
@@ -19,3 +19,25 @@ m2 = (+) <$> hurr <*> durr
 m3 :: Integer -> Integer
 m3 = liftA2 (+) hurr durr
 
+hurrDurr :: Integer -> Integer 
+hurrDurr = do
+    a <- hurr
+    b <- durr
+    return (a + b)
+
+-- Short Exercise
+
+cap :: [Char] -> [Char]
+cap xs = map toUpper xs
+
+rev :: [Char] -> [Char]
+rev xs = reverse xs
+
+composed :: [Char] -> [Char]
+composed = rev . cap
+
+fmapped :: [Char] -> [Char]
+fmapped = fmap rev cap
+
+tupled :: [Char] -> ([Char], [Char])
+tupled = (,) <$> rev <*> cap 
