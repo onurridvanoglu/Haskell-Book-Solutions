5c86eaf6bafb1b546cf5efaa0503479b306b22f4
Onur Rıdvanoğlu <onurridvanoglu@Onurs-MacBook-Pro.local>
2019-05-22 21:45:31 +0300

    add reader examples

--- Exercises/Ch-22(Reader)/reader.hs
+++ Exercises/Ch-22(Reader)/reader.hs
@@ -7,3 +7,15 @@ durr = (+10)
 
 m :: Integer -> Integer
 m = hurr . durr
+
+-- Here fmap composes two functions before applyİng 
+-- them to the argument
+m' :: Integer -> Integer
+m' = fmap hurr durr
+
+m2 :: Integer -> Integer
+m2 = (+) <$> hurr <*> durr
+
+m3 :: Integer -> Integer
+m3 = liftA2 (+) hurr durr
+
