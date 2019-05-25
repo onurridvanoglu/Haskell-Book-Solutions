33565ec8952ef0f4309a61e7872b5ea33624132c
Onur Rıdvanoğlu <onurridvanoglu@Onurs-MacBook-Pro.local>
2019-05-25 16:44:23 +0300

    complete Reader(?)

--- Exercises/Ch-22(Reader)/reader.hs
+++ Exercises/Ch-22(Reader)/reader.hs
@@ -109,3 +109,26 @@ foo r = fmap (+1) r
 bar :: Foldable f => t -> f a -> (t, Int)
 bar r t = (r, length t)
 
+froot :: Num a => [a] -> ([a], Int)
+froot r = (map (+1) r, length r)
+
+barOne :: Foldable t => t a -> (t a, Int)
+barOne r = (r, length r)
+
+frooty :: Num a => [a] -> ([a], Int)
+frooty r = bar (foo r) r
+
+frooty' :: Num a => [a] -> ([a], Int)
+frooty' = \r -> bar (foo r) r
+
+-- We abstract this out so it's not specific to these functions
+-- Here r represents the argument that both of our functions are waiting on
+fooBind :: (r -> a) -> (a -> r -> b) -> (r -> b)
+fooBind m k = \r -> k (m r) r
+
+getDogRM :: Person -> Dog
+getDogRM = do
+    name <- dogName
+    addy <- address
+    return $ Dog name addy
+
