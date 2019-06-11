4c947092c3044c0bd5e3c5db154c158cbfc25680
Onur Rıdvanoğlu <onurridvanoglu@Onurs-MacBook-Pro.local>
2019-06-11 17:32:21 +0300

    complete Compose and Identity classes

--- Exercises/Ch-25(Composing-types)/composingtypes.hs
+++ Exercises/Ch-25(Composing-types)/composingtypes.hs
@@ -1,6 +1,26 @@
 -- Identity 
 newtype Identity a = Identity { runIdentity :: a }
 
+instance Functor Identity where
+    fmap f (Identity a) = Identity (f a)
+
 --Compose 
 newtype Compose f g a = Compose { getCompose :: f (g a) } deriving (Eq, Show)
+-- We have to fmap twice to get to that value inside because of the layered structures.
+-- f and g both have to be the part of the structure that we're lifting over
+-- so they both have to be Functors themselves.
+instance (Functor f, Functor g) => Functor (Compose f g) where
+    fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
+
+-- We can generalize this to different amounts of structure, such as with one less bit of structure. 
+newtype One f a = One (f a) deriving (Eq, Show)
+
+instance (Functor f) => Functor (One f) where
+    fmap f (One fa) = One $ fmap f fa
+
+-- Or one more layer of structure than Compose:
+newtype Three f g h a = Three (f (g (h a))) deriving (Eq, Show)
+
+instance (Functor f, Functor g, Functor h) => Functor (Three f g h) where
+    fmap f (Three fgha) = Three $ (fmap . fmap . fmap) f fgha
 
