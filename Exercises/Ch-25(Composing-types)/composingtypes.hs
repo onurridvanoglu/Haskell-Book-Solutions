9b7e7b38bb29d68b5d88c86f961ade9086dcd093
Onur Rıdvanoğlu <onurridvanoglu@Onurs-MacBook-Pro.local>
2019-06-14 16:45:41 +0300

    complete composing types

--- Exercises/Ch-25(Composing-types)/composingtypes.hs
+++ Exercises/Ch-25(Composing-types)/composingtypes.hs
@@ -45,3 +45,18 @@ newtype Three f g h a = Three (f (g (h a))) deriving (Eq, Show)
 
 instance (Functor f, Functor g, Functor h) => Functor (Three f g h) where
     fmap f (Three fgha) = Three $ (fmap . fmap . fmap) f fgha
+
+-- Monad transformers - IdentityT and its instances
+newtype IdentityT f a = IdentityT { runIdentityT :: f a } deriving (Eq, Show)
+
+instance (Functor m) => Functor (IdentityT m) where
+    fmap f (IdentityT fa) = IdentityT $ fmap f fa
+
+instance (Applicative m) => Applicative (IdentityT m) where
+    pure = IdentityT . pure
+    (IdentityT fab) <*> (IdentityT fa) = IdentityT (fab <*> fa)
+
+instance (Monad m) => Monad (IdentityT m) where
+    return = pure
+    (>>=) :: IdentityT m a -> (a -> IdentityT m b) -> IdentityT m b
+    (IdentityT ma) >>= f = IdentityT $ ma >>= runIdentityT . f
