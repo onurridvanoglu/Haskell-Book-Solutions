c7cde33d8c2b9c034dbe3e362b5e85fb94cdeb58
Onur Rıdvanoğlu <onurridvanoglu@Onurs-MacBook-Pro.local>
2019-06-21 13:20:43 +0300

    add EitherT

--- Exercises/Ch-26(Monad-Transforms)/monadT.hs
+++ Exercises/Ch-26(Monad-Transforms)/monadT.hs
@@ -1,4 +1,4 @@
--- Maybe Transform
+-- MaybeT
 newtype MaybeT m a = MaybeT { runMaybeT :: m (Maybe a) }
 
 instance (Functor m) => Functor (MaybeT m) where
@@ -18,3 +18,33 @@ instance (Monad m) => Monad (MaybeT m) where
             case v of
                 Nothing -> return Nothing
                 Just y -> runMaybeT (f y)
+
+-- EitherT 
+newtype EitherT e m a = EitherT { runEitherT :: m (Either e a) }
+
+instance (Functor m) => Functor (EitherT e m) where
+    fmap f (EitherT ema) = EitherT $ (fmap . fmap) f ema
+
+instance (Applicative m) => Applicative (EitherT e m) where
+    pure = EitherT . pure . pure
+    (EitherT f) <*> (EitherT ema) = EitherT $ (<*>) <$> f <*> ema
+
+instance (Monad m) => Monad (EitherT e m) where
+    return = pure 
+    (EitherT ema) >>= f = EitherT $ do
+        a <- ema
+        case a of
+            Right x -> runEitherT (f x)
+            Left y -> return $ Left y
+
+-- 4
+swapEither :: Either a b -> Either b a
+swapEither (Left a) = (Right a)
+swapEither (Right a) = (Left a)
+
+swapEitherT :: (Functor m) => EitherT e m a -> EitherT a m e
+swapEitherT (EitherT ema) = EitherT $ fmap swapEither ema
+
+-- 5
+eitherT :: Monad m => (a -> m c) -> (b -> m c) -> EitherT a m b -> m c
+eitherT f g (EitherT ema) = undefined
